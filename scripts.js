4e9ddf26fa32c8c1c01fc995d784a71ca3ed943d
Bilal1998a <101061169+Bilal1998a@users.noreply.github.com>
2023-06-18 23:15:08 +0200

    Upload files

---
+++ scripts.js
@@ -0,0 +1,36 @@
+const url = 'https://www.bilalahmed.no/cms/wp-json/wp/v2/posts?_embed=wp:featuredmedia&per_page=10';
+const slider = document.querySelector(".card-container")
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+renderBlogPosts(data)
+})
+.catch(error => console.log(error))
+
+function renderBlogPosts(data) {
+
+    slider.innerHTML = "";
+
+    data.forEach(post => {
+        const {
+            id, title, excerpt, _embedded
+        } = post; 
+
+        const featured_image = _embedded["wp:featuredmedia"][0].source_url;
+    slider.innerHTML +=` 
+    <div class="card">
+            <img src="${featured_image}" alt="Image 1" />
+            <h2>${title.rendered}</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+              velit dolores ab ducimus at quia amet harum rem dolore fugiat
+              pariatur
+            </p>
+            
+            <a href="/specific.html?id=${id}" class="blogspes">MORE</a>
+            
+            
+      </div>
+    ` })
+}
---
+++ specific.js
@@ -0,0 +1,48 @@
+
+const slider = document.querySelector(".card-container")
+const queryString = document.location.search;
+const blogContainer = document.querySelector(".blogspes")
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get('id');
+const url = `https://www.bilalahmed.no/cms/wp-json/wp/v2/posts/${id}?_embed=wp:featuredmedia`;
+https://www.bilalahmed.no/cms/wp-json/wp/v2/posts?_embed=wp:featuredmedia&per_page=10
+
+console.log(id)
+
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+specific(data)
+
+})
+.catch(error => console.log(error))
+
+
+
+
+
+function specific(data){
+const {title, _embedded, excerpt, content} = data
+const media = _embedded["wp:featuredmedia"][0].source_url;
+console.log(media)
+console.log(excerpt.rendered)
+
+blogContainer.innerHTML = `
+<img class="expo" src="${media}" alt="blog picture" />
+      <section>
+        <h1 class="expotext">${title.rendered}</h1>
+        
+        
+
+        <p class="specific-text">
+        ${content.rendered}
+        </p>
+        
+
+`
+
+}
+
